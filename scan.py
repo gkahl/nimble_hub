a1e97b729f558e575594af6f03de17a569979974
gkahl <gkahl@gwu.edu>
2019-04-03 15:39:51 -0400

    Add files via upload

---
+++ scan.py
@@ -0,0 +1,17 @@
+#zbar-py and opencv_python
+import numpy as np
+import cv2
+import zbar
+
+
+img = cv2.imread('/home/greg/opencv_project/3qr.jpg', 0)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+scanner = zbar.Scanner()
+results = scanner.scan(img)
+for result in results:
+	print(result.type, result.data, result.quality, result.position)
+
