49a448071f2ac3de237da9b72df9da6bc1cab889
gkahl <gkahl@gwu.edu>
2019-05-01 16:57:51 -0400

    Added multiple EAN-13 barcode detection to scan.py and added additional barcodes to the pictures directory

--- scan.py
+++ scan.py
@@ -2,9 +2,15 @@
 import numpy as np
 import cv2
 import zbar
+import sys
 
 
-img = cv2.imread('/home/greg/opencv_project/3qr.jpg', 0)
+if len(sys.argv) > 1:
+    path = ('/home/greg/nimble_hub/pictures/' + sys.argv[1])
+else:
+    path = '/home/greg/nimble_hub/pictures/adj_2.png'
+
+img = cv2.imread(path, 0)
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
@@ -13,5 +19,18 @@ cv2.destroyAllWindows()
 scanner = zbar.Scanner()
 results = scanner.scan(img)
 for result in results:
-	print(result.type, result.data, result.quality, result.position)
+    xval, yval = list(zip(*result.position))
+    
+    count = 1
+    
+    for i in range(len(yval)):
+        if ((len(yval) - i) > 6):
 
+            if ((yval[i] != yval[i+1]) and (yval[i+1] == yval[i+2])):
+                if (yval[i+3] != yval[i+2] and yval[i+3] == yval[i+4] and yval[i+3] == yval[i+5] and yval[i+3] == yval[i+6]):
+                    count = count+1
+            if yval[i+1] > yval[i] + 2:
+                count = count + 1
+    print(result.type, result.data, result.quality,'Count:', count)
+        
+        
