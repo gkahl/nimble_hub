997ce14523bfd6430bec90b3d523387c225a6f26
gkahl <gkahl@gwu.edu>
2019-05-01 13:37:03 -0900

    Added a new class to scan.py to hold the barcode information to be sent to the API

--- scan.py
+++ scan.py
@@ -3,19 +3,29 @@ import numpy as np
 import cv2
 import zbar
 import sys
+from collections import Counter
+
+
+class sned(object):
+    def __init__(self,code_type,data,count):
+        self.code_type = code_type
+        self.data = data
+        self.count=count
+
+ToSend = list()
 
 
 if len(sys.argv) > 1:
-    path = ('/home/greg/nimble_hub/pictures/' + sys.argv[1])
+    path = ('/home/pi/nimble_hub/pictures/' + sys.argv[1])
 else:
-    path = '/home/greg/nimble_hub/pictures/foo.jpg'
+    path = '/home/pi/nimble_hub/pictures/foo.jpg'
 
 img = cv2.imread(path, 0)
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
+results2=list()
 scanner = zbar.Scanner()
 results = scanner.scan(img)
 for result in results:
@@ -31,6 +41,14 @@ for result in results:
                     count = count+1
             if yval[i+1] > yval[i] + 2:
                 count = count + 1
-    print(result.type, result.data, result.quality,'Count:', count)
-        
-        
+    if(result.type != 'QR-Code'):
+        new_sned=sned(result.type, result.data, count) 
+        ToSend.append(new_sned) 
+        for item in ToSend: 
+            print(item.code_type, item.data, item.count)
+    else:
+        results2.append(result.data)
+qr_count = Counter(results2)
+print(qr_count)
+	
+
